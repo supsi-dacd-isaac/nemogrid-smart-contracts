98ff96ec94730f1612a9944d40923ec0f78fa700
Davide Strepparava <davide.strepparava@supsi.ch>
2018-12-13 15:47:40 +0100

    Added inputs args printing and .gitignore updating

--- migrations/3_groupsmanager_deployment.js
+++ migrations/3_groupsmanager_deployment.js
@@ -1,3 +1,5 @@
+var abi = require('ethereumjs-abi')
+
 var SafeMath = artifacts.require('../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol');
 var NGT = artifacts.require('./NGT.sol');
 var GroupsManager = artifacts.require('./GroupsManager.sol');
@@ -7,4 +9,10 @@ module.exports = function (deployer, network, accounts) {
     deployer.deploy(SafeMath);
     deployer.link(SafeMath, GroupsManager);
     deployer.deploy(GroupsManager, NGT.address);
+
+    var inputTypes = ["address"];
+    var inputArgs = [NGT.address];
+    var inputArgsEnc = abi.rawEncode(inputTypes, inputArgs);
+
+    console.log('\tGroupsManager input args: ' + inputArgsEnc.toString('hex') + '\t');
 };
